541298eed6c861f4073ad96dbe75e3c1710f6a27
AbhinavXT <abhinavpathaka17@gmail.com>
2022-09-05 22:46:00 +0530

    Main page

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -5,7 +5,7 @@ const signup = () => {
     <div className='flex flex-col justify-center -mt-12 gp-y-20 mb-24'>
         <div className='bg-gray-900 text-gray-200 text-3xl font-semibold py-20 pl-32'>Signup / Login</div>
         
-        <div className='flex mt-16 justify-center bg-gray-100 py-12 mx-auto divide-x divide-gray-400 rounded'>
+        <div className='flex mt-16 justify-center bg-gray-100 py-12 mx-auto divide-x divide-gray-400 rounded shadow-lg'>
             <div className='flex flex-col items-center justify-center gap-y-8 px-12 text-center'>
                 <div>
                     <Image
@@ -34,7 +34,7 @@ const signup = () => {
                         <input className="bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your email" />
                     </form>
                     <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                         </svg>
                     </button>
