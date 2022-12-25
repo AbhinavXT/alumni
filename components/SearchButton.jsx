86155ab518ca987c3a0300edc157916484a1ca29
AbhinavXT <abhinavpathaka17@gmail.com>
2022-12-25 19:20:57 +0530

    updated number of registered people

--- components/SearchButton.jsx
+++ components/SearchButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchButton = ({ expanded, text }) => {
     return (
-        <div className='flex justify-between py-4 px-8 bg-gray-300 text-lg rounded-sm cursor-pointer'>
+        <div className='flex justify-between py-4 px-6 w-84 bg-gray-300 text-lg rounded-sm cursor-pointer'>
             <div className='font-bold text-left'>{text}</div>
         </div>
     )
